0b050339647f541e0d0c42dfaa7460706fc1eae6
SeanEdwards2021 <SeanE301292@yahoo.com>
2021-10-06 00:40:18 +0100

    added onLoad function call.

--- Frontend/assets/js/productPage.js
+++ Frontend/assets/js/productPage.js
@@ -13,6 +13,8 @@ let productQuantity = document.querySelector("#productCounterText");
 const btnAddToCart = document.querySelector(".addToCart");
 let product = {};
 
+window.onload = function() {init()};
+
 // ---------- FUNCTIONS TO RUN WHEN PAGE IS LOADING ----------
 function init() {
   let productId = getProductId();
@@ -155,5 +157,24 @@ function addNumCart() {
   cartNum.innerHTML = totalQuantityOfProducts;
 }
 
-// ---------- CALLING PAGE FUNCTIONS ----------
-init()
+// SCROLL TO TOP OF PAGE FEATURE
+
+//Scroll to top button
+let myButton = document.querySelector("#scrollButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
