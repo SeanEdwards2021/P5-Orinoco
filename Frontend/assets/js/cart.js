c0eb15c8af445e96a72f1309a1576f52b51ef324
SeanEdwards2021 <SeanE301292@yahoo.com>
2021-10-06 04:02:41 +0100

    created emptyCart, removeAllItems, removeSingleProcuct functions.

--- Frontend/assets/js/cart.js
+++ Frontend/assets/js/cart.js
@@ -5,28 +5,50 @@ const cartContainer = document.querySelector('#cartContainer')
 window.onload = function() {init()};
 
 function init() {
-  cartProductList ()
-  numberItemsInCart ()
-  orderSubtotal()
-  addNumCart()
+  console.log("Working")
+
+  if (!localStorageContent) {
+    emptyCart()
+  } else if( cartItems.length === 0){
+    emptyCart()
+  } else { 
+    cartProductList ()
+    orderSubtotal()
+    addNumCart()
+  }
 }
 
-function numberItemsInCart() {
+function emptyCart() {
+  let section = document.querySelector('.cartSection')
+  section.innerHTML = `
+    <div class="container card">
+      <div class="row shadow">
+        <div class="col-md-12 mt-5">
+          <div id="cart-products">
+            <div class="text-center">
+              <i class="fas fa-shopping-basket h1" style="background:-webkit-linear-gradient(30deg, rgba(206,126,73,1) 32%, rgba(144,91,250,1) 100%);-webkit-background-clip: text;-webkit-text-fill-color:transparent;" aria-hidden="true"></i>
+            </div>
+            <h2 class="text-center font-weight-bold my-5">Your Cart Is Currently Empty</h2>
+            <p class="text-center lead">Before proceed to checkout you must add some products to your shopping cart.<br>
+            You will find a lot of interesting products on our Homepage.</p>
+            <a href="../../index.html" style="text-decoration:none;">
+              <button type="button" class="btn btn-outline-primary mx-auto d-block mb-5">Return to shop 
+                <i class="fas fa-reply" aria-hidden="true"></i>
+              </button>
+            </a>
+          </div>     
+        </div>
+      </div>
+    </div>`;
+}
 
-  let numberItemsInCart = document.querySelector('.numberItemsInCart')
-  let totalNumberItemsInCart = 0
-
-  // Loops through each object of API and extract the product data
-  for (let i = 0; i < cartItems.length; i++) {
-
-    // Product Value Variables
-    let productQuantity = cartItems[i].quantity
-    totalNumberItemsInCart += productQuantity
-  }
-  numberItemsInCart.innerHTML = "Total Items: " + totalNumberItemsInCart
+function removeAllItems() {
+  localStorage.clear();
+  emptyCart()
 }
 
 function cartProductList() {
+  cartContainer.innerHTML = ''
 
   // Loops through each object of API and extract the product data
   for (let i = 0; i < cartItems.length; i++) {
@@ -41,38 +63,49 @@ function cartProductList() {
     let totalProductPrice = (productPrice * productQuantity);
 
     // Create & Append New Product
+    
     let product = document.createElement('div');
-    product.classList.add('row');
-
+    product.classList.add('row', 'productContainer', 'shadow');
     product.innerHTML = `
-      <div class="col-md-5 col-11 mx-auto bg-light d-flex justify-content-center align-items-center shadow product_img">
-        <img src="${productImg}" class="img-fluid" alt="cart img">
+      <div id="${i}" class="productContainerImage col-md-5 col-11 mx-auto">
+        <img src="${productImg}" class="productImage" alt="cart img">
       </div>
       <div class="col-md-7 col-11 mx-auto px-4 mt-2">
         <div class="row">
-          <div class="col-6 card-title">
-          <h4 class="mb-4 product_name">${productName}</h4>
-          <p class="mb-2">${productLens}</p>
-        </div>
-        <div class="col-6">
-          <div id="productCounter">
-            <p><i class="fas fa-trash-alt"></i>Remove Product</p>
-          </div>
+          <div class="col-6">
+          <h4 class="mb-4 productTitle">${productName}</h4>
+          <p class="mb-2 productLens">${productLens}</p>
         </div>
       </div>
-      <div class="row">
-        <div class="col-8 d-flex justify-content-between remove_wish">
-          <button class="btn btn-outline-primary" id="productCounterDecrease">-</button>
-          <span id="productCounterText" class="counter-text">${productQuantity}</span>
-          <button class="btn btn-outline-primary" id="productCounterIncrease">+</button>
+      <div class="row productContainerMoreDetails">
+        <div class="productContainerItemQuantity col-4">
+          <button class="productItemPlus btn btn-outline-primary" id="productCounterDecrease">-</button>
+          <span id="productCounterText" class="productItemQuantity">${productQuantity}</span>
+          <button class="productItemMinus btn btn-outline-primary" id="productCounterIncrease">+</button>
         </div>
-        <div class="col-4 d-flex justify-content-end price_money">
-          <h3>£<span id="itemval">${totalProductPrice}</span></h3>
+        <div class="col-4 productContainerItemPrice">
+          <h3 class="productItemPrice">£<span>${totalProductPrice}</span></h3>
+        </div>
+        <div id="col-4 productContainerRemoveItem">
+          <button id="${i}" class="productRemoveItemButton btn btn-danger btn-sm mt-3" onClick="removeSingleProduct(this.id)">Remove Item</button>
         </div>
       </div>`;
       cartContainer.appendChild(product);
   }
 }
+
+function removeSingleProduct(productID) {
+  console.log(cartItems)
+  cartItems.splice(productID, 1);
+  console.log(cartItems)
+  localStorage.setItem('cart', JSON.stringify(cartItems));
+  init()
+}
+
+function changeProductQuantity() {
+  console.log("Hello")
+}
+
 
 function orderSubtotal() {
 
