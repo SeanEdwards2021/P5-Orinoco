67deac13f6df36e91b4edb905be82b8f9aad5e8a
SeanEdwards2021 <SeanE301292@yahoo.com>
2021-10-07 22:35:57 +0100

    Removed repeated code.

--- Frontend/assets/js/orderConfirmation.js
+++ Frontend/assets/js/orderConfirmation.js
@@ -31,7 +31,6 @@ function init() {
   cartProductList()
 }
 
-
 function formDetails() {
   firstName.innerHTML = "Hello, " + customers[0].firstName
   orderDate.innerHTML = customers[0].orderPlaced
@@ -85,35 +84,7 @@ function cartProductList() {
   }
 }
 
-
-
-
-
-
 // Removes all items currently saved on local storage and updates cart page
 function removeAllItems() {
   localStorage.clear();
-  addNumCart()
-}
-
-// ---------- SCROLL TO TOP OF PAGE FEATURE ----------
-
-//Scroll to top button
-let myButton = document.querySelector("#scrollButton");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    myButton.style.display = "block";
-  } else {
-    myButton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
 }
--- Frontend/assets/js/productPage.js
+++ Frontend/assets/js/productPage.js
@@ -145,28 +145,6 @@ btnAddToCart.addEventListener('click', () => {
   addNumCart()
 });
 
-// ---------- SCROLL TO TOP OF PAGE FEATURE ----------
-
-//Scroll to top button
-let myButton = document.querySelector("#scrollButton");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    myButton.style.display = "block";
-  } else {
-    myButton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 // ---------- GET THE CURRENT CART ITEM NUMBER FOR THE NAVBAR CART ----------
 
 function addNumCart() {
